5c753b97c035d704a0ea629e7ccf32cad5a18910
jjang-ye-ji <ab1321@naver.com>
2020-05-15 11:27:00 +0900

    제이쿼리 서브메뉴

---
+++ script/just.js
@@ -0,0 +1,26 @@
+$(function(){
+    gnb();
+});
+function gnb(){
+    // 메인메뉴 마우스 오버하면 서브메뉴를 부드럽게 등장
+    // 마우스 오버한 메인메뉴는 하이라이트 적용
+    // $("#gnb > li").mouseover(function(){
+    //     $(this).addClass("on");
+    //     $(".lnb").show();
+    // });
+    // $("#gnb > li").mouseout(function(){
+    //     $(this).removeClass("on");
+    //     $(".lnb").hide();
+    // });
+
+    $("#gnb > li").hover(
+        function(){
+            $(this).addClass("on");
+            $(".lnb").show();
+        },
+        function(){
+            $(this).removeClass("on");
+            $(".lnb").hide();
+        }
+    );
+}
